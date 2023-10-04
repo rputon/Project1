0f2bad149ae3fcf9f1d9c18ab2a6ab510e8be395
rputon <roberta.puton@gmail.com>
2023-10-04 08:31:19 -0300

    Aula de 3-10

---
+++ src/Instituto.java
@@ -0,0 +1,22 @@
+
+public class Instituto {
+	private String nome;
+	private int cep;
+	
+	public Instituto() {
+		
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCep() {
+		return cep;
+	}
+	public void setCep(int cep) {
+		this.cep = cep;
+	}
+}
